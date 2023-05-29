b546ce9ad065a4a0c7eeb3cd2c3624a0305b06fe
unexpectedhaki <tpritchard843@gmail.com>
2023-05-29 17:23:33 -0400

    Layed out constructor methods

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 
 class Calculator {
     constructor () {
-        
+        this.divide = function() {
+            console.log('divide');
+        }
+        this.multiply = function() {
+            console.log('multiply');
+        }
+        this.add = function() {
+            console.log('add');
+        }
+        this.subtract = function() {
+            console.log('subtract')
+        }
     }
 }
